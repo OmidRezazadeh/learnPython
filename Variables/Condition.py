d17646bb9ea635d848afad37240a735d99e55508
orprogrammer <orprogrammer@gmail.com>
2023-06-08 22:24:09 +0430

    learn condition

---
+++ Variables/Condition.py
@@ -0,0 +1,13 @@
+a = 33444
+b = 331
+c = 2
+# if a < b:
+#     print("b is greater than a")
+# elif a > b:
+#     print("else")
+# else:
+# print("test")
+if a < b:
+    pass
+else:
+    print("ooikkkkk")
---
+++ Variables/app.py
@@ -0,0 +1,24 @@
+# age =30
+# price =20
+# first_name="omid"
+# is_online=True
+# print(f"my name is  {first_name} my age is {age} and my car price is {price} status online {is_online}")
+
+# birth_year = input("enter your birth year:")
+# age = 2020 - int(birth_year)
+# print(age)
+# First = input("first number")
+# second = input("second")
+# print(int(second) + float(First))
+
+
+# course = "python for start"
+# print('for' in course)
+
+# print(10 / 3)
+# print(8 % 2)
+# print(10 ** 3)
+# print(10 * 3)
+
+x = (10 + 3) * 2
+print(x)
