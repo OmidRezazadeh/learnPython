657b3b8a13aeb75f5c4c893c9e75e85163fca706
orprogrammer <orprogrammer@gmail.com>
2023-06-11 23:25:42 +0430

    learn list and for

---
+++ Variables/List.py
@@ -0,0 +1,28 @@
+Fruits = ["kiwi", "apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+musicBand = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+# print(Fruit)
+# print(len(Fruit))
+# print(type(Fruit))
+# print(thisList)
+# print(Fruit[-4:-1])
+# if "omid" in Fruit:
+#     print("yes")
+# else:
+#     print("no")
+# Fruit[1:4] = ["blackcurrant"]
+# Fruit.insert(0, "test")
+# Fruit.append("okif")
+# Fruit.extend(musicBand)
+# Fruit.remove("banana")
+# Fruit.pop(1)
+# Fruit.pop()
+# print(Fruit)
+# for Fruit in Fruits:
+#     print(Fruit[1])
+# for Fruit in Fruits:
+#     if "a" in Fruit:
+#         newList.append(Fruit)
+# print(newList)
+newList = Fruits + musicBand
+# Fruits.sort(reverse=True)
+print(newList)
