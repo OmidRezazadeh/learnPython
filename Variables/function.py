c6dae6e7e56d0ba89080f6728024cbe57662a742
orprogrammer <orprogrammer@gmail.com>
2023-06-12 17:55:28 +0430

    learn list function

---
+++ Variables/function.py
@@ -0,0 +1,35 @@
+# def myFunction():
+#     print("hello")
+#
+#
+# myFunction()
+
+
+# def argFunction(Fname):
+#     print(Fname + " test")
+#
+#
+# argFunction("email")
+# argFunction("omid")
+# argFunction("reza")
+
+
+# def muFunction(*kids):
+#     print("the youngest " + kids[0])
+#
+#
+# muFunction("Emil", "Tobias", "Linus")
+# def myFunction(child1, child2, child3):
+#     print("The youngest child is " + child3)
+#
+#
+# myFunction(child1="Emil", child2="Tobias", child3="Linus")
+
+def my_function(fruits):
+    for fruit in fruits:
+        print(fruit)
+
+
+fruits = ["apple", "banana", "cherry"]
+
+my_function(fruits)
